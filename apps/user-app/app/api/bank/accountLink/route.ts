416bc2875e8f2c7fd92248610f5968265e0ea7cb
rajuperikala01 <rajuperikala01@gmail.com>
2025-01-19 11:05:40 +0530

    removed api from gitignoe

---
+++ apps/user-app/app/api/bank/accountLink/route.ts
@@ -0,0 +1,97 @@
+import prisma from "@repo/database/client";
+import axios, { AxiosError } from "axios";
+import { NextRequest, NextResponse } from "next/server";
+
+export const POST = async (req: NextRequest) => {
+  const userId = await req.json();
+  try {
+    const user = await prisma.user.findUniqueOrThrow({
+      where: {
+        id: Number(userId.id),
+      },
+    });
+
+    if (user?.bankCustomerId) {
+      return NextResponse.json(
+        {
+          message: "Already Linked account",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    try {
+      const request = await axios.post(
+        `${process.env.NEXT_PUBLIC_BANK_URL}/BankLink`,
+        {
+          number: user?.number,
+        }
+      );
+
+      if (request.status === 200) {
+        try {
+          await prisma.user.update({
+            where: {
+              id: user?.id,
+            },
+            data: {
+              bankCustomerId: request.data.customerId,
+            },
+          });
+
+          return NextResponse.json(
+            {
+              message: "Successfully Added your Account",
+            },
+            { status: 200 }
+          );
+        } catch (error) {
+          return NextResponse.json(
+            {
+              error: "unable to process your request",
+            },
+            { status: 500 }
+          );
+        }
+      }
+    } catch (error: any) {
+      if (error instanceof AxiosError) {
+        if (error.code === "ECONNREFUSED" || error.code === "ENOTFOUND") {
+          return NextResponse.json(
+            {
+              error: "Couldn't connect with the Bank server",
+            },
+            { status: 500 }
+          );
+        }
+        if (error.status === 401) {
+          return NextResponse.json({
+            error: error.response?.data.error,
+          });
+        }
+        return NextResponse.json(
+          {
+            error: error.response?.data.error,
+          },
+          { status: error.response?.status }
+        );
+      }
+      return NextResponse.json(
+        {
+          error:
+            "An Unexpected Error Occurred Please try again after some time",
+        },
+        { status: 500 }
+      );
+    }
+  } catch (error) {
+    return NextResponse.json(
+      {
+        error: "User not Found",
+      },
+      { status: 400 }
+    );
+  }
+};
---
+++ apps/user-app/app/api/payments/send/route.ts
@@ -0,0 +1,154 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../../lib/auth";
+import { NextRequest, NextResponse } from "next/server";
+import { p2pTransfer } from "@repo/validation/bankschemas";
+import prisma, { Prisma } from "@repo/database/client";
+
+export const POST = async (req: NextRequest) => {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return NextResponse.json(
+      {
+        error: "Unauthenticated Request",
+      },
+      { status: 403 }
+    );
+  }
+
+  const data = await req.json();
+
+  const validation = p2pTransfer.safeParse(data);
+  if (validation.error) {
+    return NextResponse.json(
+      {
+        error: validation.error.issues[0]?.message,
+      },
+      { status: 401 }
+    );
+  }
+
+  try {
+    const fromUser = await prisma.user.findUniqueOrThrow({
+      where: {
+        id: Number(session.user.id),
+      },
+    });
+
+    if (fromUser.number === validation.data.to) {
+      return NextResponse.json(
+        {
+          error: "Invalid Transaction",
+        },
+        { status: 400 }
+      );
+    }
+
+    if (fromUser && fromUser.Balance / 100 < validation.data.amount) {
+      try {
+        const toUser = await prisma.user.findUniqueOrThrow({
+          where: {
+            number: validation.data.to,
+          },
+        });
+        await prisma.walletTransactions.create({
+          data: {
+            amount: validation.data.amount * 100,
+            senderId: fromUser.id,
+            reciverId: toUser.id,
+            status: "Failed",
+            createdAt: new Date(),
+          },
+        });
+        return NextResponse.json(
+          {
+            error: "Insufficient Funds",
+          },
+          { status: 400 }
+        );
+      } catch (error) {
+        return NextResponse.json(
+          {
+            error: "Insufficient Funds",
+          },
+          { status: 400 }
+        );
+      }
+    }
+
+    try {
+      const toUser = await prisma.user.findUniqueOrThrow({
+        where: {
+          number: validation.data.to,
+        },
+      });
+
+      try {
+        await prisma.$transaction(async (tx: Prisma.TransactionClient) => {
+          await tx.$queryRaw`SELECT * FROM "User" WHERE "id" = ${fromUser?.id} FOR UPDATE`;
+
+          await tx.user.update({
+            where: {
+              id: fromUser?.id,
+            },
+            data: {
+              Balance: {
+                decrement: validation.data.amount * 100,
+              },
+            },
+          });
+
+          await tx.user.update({
+            where: {
+              id: toUser?.id,
+            },
+            data: {
+              Balance: {
+                increment: validation.data.amount * 100,
+              },
+            },
+          });
+          if (fromUser && toUser) {
+            await tx.walletTransactions.create({
+              data: {
+                amount: validation.data.amount * 100,
+                status: "Success",
+                createdAt: new Date(),
+                senderId: fromUser.id,
+                reciverId: toUser.id,
+              },
+            });
+          }
+        });
+
+        return NextResponse.json(
+          {
+            message: "Successful",
+          },
+          { status: 200 }
+        );
+      } catch (error) {
+        return NextResponse.json(
+          {
+            error: "Internal Server Error, please try again later",
+          },
+          { status: 500 }
+        );
+      }
+    } catch (error) {
+      return NextResponse.json(
+        {
+          error: "User with this mobile number doesn't Exists",
+        },
+        { status: 400 }
+      );
+    }
+  } catch (error) {
+    return NextResponse.json(
+      {
+        error: "User not Found",
+      },
+      { status: 400 }
+    );
+  }
+};
